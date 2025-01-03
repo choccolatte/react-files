22ea42e1c48fff382f9b95d08c89898ddf7e96df
A Sin <124335907+choccolatte@users.noreply.github.com>
2025-01-04 00:46:52 +0530

    changes made to app.js

--- reactdev/src/App.js
+++ reactdev/src/App.js
@@ -2,24 +2,37 @@ export default function Board(){
 	return (
 		<>
 		<div className="board-row">
+		<Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
         <Square />
         <Square />
         <Square />
       </div>
       <div className="board-row">
-	  <Square />
-	  <Square />
-	  <Square />
-      </div>
-      <div className="board-row">
-	  <Square />
-	  <Square />
-	  <Square />
+        <Square />
+        <Square />
+        <Square />
       </div>	
 		</>
 	)
 }
 
-function Square({value}){
-	return <button className="square">{value}</button>
+import { useState } from "react"
+
+function Square(){
+	const [value, setValue] = useState(null);
+
+	function handleClick(){
+		setValue('X')
+	}
+	
+	return(
+	<button className="square" 
+	onClick={handleClick}>
+	{value}
+	</button>
+	)
 }
