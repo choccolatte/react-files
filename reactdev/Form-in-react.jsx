032c5d646fe8fe4036e502db64e3ae7018aee569
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-02-18 15:54:28 +0530

    forms- 180225

---
+++ reactdev/Form-in-react.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+// function MyForm(){
+//     const [name, setName] = useState('')
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault()
+//         alert(`The name you entered was: ${name}`)
+//     }
+
+//     return (
+//         <form onSubmit={handleSubmit}>    
+//             <label>Enter your name:
+//                 <input type='text' value = {name} onChange = {(e) => setName(e.target.value)} />
+//               </label>
+
+//               <input type = 'submit' />
+//         </form>
+//     )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(<MyForm />)
+
+
+// form with two input areas - 
+
+function MyForm(){
+    const [inputs, setInputs] = useState({})
+
+    const handleChange = (event) => {
+        const name = event.target.name; 
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert(inputs)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>    
+            <label>Enter your name:
+                <input 
+                    type='text' 
+                    name='username'
+                    value = {inputs.username || ''} 
+                    onChange = {handleChange} />
+              </label>
+
+            <label>Enter your phone number:
+                <input 
+                    type='number' 
+                    name='phonenumber'
+                    value = {inputs.phonenumber || ''} 
+                    onChange = {handleChange} />
+              </label>
+
+              <input type = 'submit'>
+        </form>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<MyForm />)
