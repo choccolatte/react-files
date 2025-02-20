5bc2b26d80cfc5c5e0acf7b26c973dbdc56abc30
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-02-20 12:29:17 +0530

    Update Car.jsx

--- reactdev/Car.jsx
+++ reactdev/Car.jsx
@@ -1,16 +1,45 @@
-function Car(props){
-    return <h2>I am a {props.brand}!</h2>
-}
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-function Garage(){
-    const carName = 'Toyota' // defining the variable here
-    return(
-        <>
-            <h1>The car in my garage!</h1>
-            <Car carName='Toyota' /> // passing defined variable here
-        </>
-    )
+function Car(){
+    const [car, setCar] = setState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red"
+})
+
+    const updateColor = () => {
+        setCar (previousState => {
+            return {...previousState, color:'red'}
+        })
+    }
+
+    return (
+    <>
+        <h1>My {car.brand}</h1>
+        <p>Its a {car.color} {car.model} from {car.year}.</p>
+        <button type='button' onClick = {updateColor}>Red</button>
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Garage/>)
+root.render(<Car />)
+
+
+// function Car(props){
+//     return <h2>I am a {props.brand}!</h2>
+// }
+
+// function Garage(){
+//     return(
+//         <>
+//             <h1>The car in my garage!</h1>
+//             <Car brand='Toyota' />
+//         </>
+//     )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(<Garage/>)
